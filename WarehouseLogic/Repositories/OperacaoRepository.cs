2d4f09c603198f64f1ea066ac8eac077034bfae4
igorbdaluz <igorbdaluz@hotmail.com>
2024-10-12 15:05:58 -0300

    Adicionando o modelo Operacao ao padrão Repository.

---
+++ WarehouseLogic/Repositories/OperacaoRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using WarehouseLib.Interfaces;
+using WarehouseLib.Models;
+using WarehouseLogic.Context;
+
+namespace WarehouseLogic.Repositories
+{
+    public class OperacaoRepository : IRepository<Operacao>
+    {
+        private readonly SQLiteDbContext _dbContext;
+
+        public OperacaoRepository(SQLiteDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(Operacao entity) => _dbContext.Add(entity);
+
+        public void Delete(int id) => _dbContext.Remove(GetById(id));
+
+        public ObservableCollection<Operacao> GetAll() => _dbContext.Set<Operacao>().Local.ToObservableCollection();
+
+        public Operacao GetById(int id) => _dbContext.Set<Operacao>().Find(id);
+
+        public void SaveChanges() => _dbContext.SaveChanges();
+
+        public void Update(Operacao entity) => _dbContext.Update(entity);
+    }
+}
