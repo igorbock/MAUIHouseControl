03c537ce40d0c47ef74e1edda1227bbfb3f7dfae
igorbdaluz <igorbdaluz@hotmail.com>
2024-10-12 15:02:33 -0300

    Adicionando o modelo Marca ao padrão Repository.

---
+++ WarehouseLogic/Repositories/MarcaRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using WarehouseLib.Interfaces;
+using WarehouseLib.Models;
+using WarehouseLogic.Context;
+
+namespace WarehouseLogic.Repositories
+{
+    public class MarcaRepository : IRepository<Marca>
+    {
+        private readonly SQLiteDbContext _dbContext;
+
+        public MarcaRepository(SQLiteDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(Marca entity) => _dbContext.Add(entity);
+
+        public void Delete(int id) => _dbContext.Remove(GetById(id));
+
+        public ObservableCollection<Marca> GetAll() => _dbContext.Set<Marca>().Local.ToObservableCollection();
+
+        public Marca GetById(int id) => _dbContext.Set<Marca>().Find(id);
+
+        public void SaveChanges() => _dbContext.SaveChanges();
+
+        public void Update(Marca entity) => _dbContext.Update(entity);
+    }
+}
