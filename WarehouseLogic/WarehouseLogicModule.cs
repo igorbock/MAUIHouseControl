a80cd9073c573ee7445dc803f304b3685007cd90
igorbdaluz <igorbdaluz@hotmail.com>
2024-10-13 18:36:03 -0300

    Adicionando o pacote Ninject e sobreescrevendo NinjectModule no projeto WarehouseLogic para associação das dependências.

---
+++ WarehouseLogic/WarehouseLogicModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ninject.Modules;
+using WarehouseLib.Interfaces;
+using WarehouseLib.Models;
+using WarehouseLogic.Context;
+using WarehouseLogic.Repositories;
+
+namespace WarehouseLogic
+{
+    public class WarehouseLogicModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<SQLiteDbContext>().ToSelf();
+            Bind<DbContextOptions>().ToSelf();
+
+            Bind<IRepository<Produto>>().To<ProdutoRepository>();
+            Bind<IRepository<Marca>>().To<MarcaRepository>();
+            Bind<IRepository<Operacao>>().To<OperacaoRepository>();
+        }
+    }
+}
